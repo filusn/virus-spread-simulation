e9b34817ea421e02615cd807c31e093e3ba5213d
Filip Noworolnik <fnoworolnik@gmail.com>
2021-12-17 18:30:24 +0100

    Add: Simulation run

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import streamlit as st
 
+from controllers import Controller
 
 # Main
 st.title('Virus Spread Simulation')
 
+
+# Sidebar # TODO: Add help to controls
+
 st.sidebar.title('Parameters')
-num_pop = st.sidebar.slider('Population size', 10, 1000, 100)
+pop_num = st.sidebar.slider('Population size', 10, 1000, 100)
 map_size_x = st.sidebar.slider('Map size x', 10, 200, 100)
 map_size_y = st.sidebar.slider('Map size y', 10, 200, 100)
 init_infected = st.sidebar.slider('Initialy infected people', 1, 20, 1)
@@ -18,3 +22,16 @@ illness_period = st.sidebar.slider('Illness period', 1, 20, 10)
 lockdown = st.sidebar.checkbox('Lockdown', value=False)
 lockdown_range = st.sidebar.slider('Lockdown steps range', 1, 1000, (50, 150)) # TODO: Check for max steps
 self_isolation = st.sidebar.checkbox('Self isolation', value=False)
+
+params = dict(pop_num=pop_num, map_size=(map_size_x, map_size_y), init_infected=init_infected, infection_dist=infection_dist, infection_prob=infection_prob,
+                    death_prob=death_probability, walking_range=(-walking_range, walking_range), incubation_period=incubation_period,
+                    illness_period=illness_period)
+
+if lockdown:
+    params['lockdown'] = lockdown_range
+
+cont = Controller(**params)
+
+steps = st.number_input('Number of steps', 10, 1000, 100)
+
+start = st.button('Start', on_click=cont.simulate, args=(steps,))
